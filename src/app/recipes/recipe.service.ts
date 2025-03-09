4fddb5fd919733cb9b4065f1bbfe9e1db8a1bc04
Monika Bloniarz <monika.bloniarz@capgemini.com>
2025-03-09 17:45:01 +0100

    inject service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
@@ -17,7 +19,13 @@ export class RecipeService {
             new Ingredient('Tomatoes', 10),])
     ];
 
+    constructor(private slService: ShoppingListService) {}
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
